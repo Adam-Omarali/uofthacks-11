da3a7ffb2f4d56e6c0fc4518f019d3fc2a7d4cac
DavidBShan <davidbshan@gmail.com>
2024-01-27 20:07:16 -0500

    idk man

--- ideas.md
+++
@@ -1,8 +0,0 @@
-- Music Video Generation
-- VR Memories
-- Rolodex Memories x RAG Journal
-- Social Media Content Classifier
-- X Wrapped
-- Boombox generator
-- dalle multiplayer
-
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
@@ -5,4 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
+    return {"message": "Hello World"}
+
+@app.get("/sound")
+async def get_sound():
     return FileResponse("./audio/test.mp3", media_type="audio/mp3")

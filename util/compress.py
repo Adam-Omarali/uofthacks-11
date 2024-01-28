f3a35d0b88bb7c2f72076bbae96b641fa5d43ecc
Adam <adamomarali37@gmail.com>
2024-01-28 07:02:56 -0500

    arduino code

--- util/compress.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def compressAudio():
-    subprocess.call(['ffmpeg', '-i', '../audio/test.mp3', '-acodec', 'pcm_s16le','-ar','8000','-ac','1','./audio/test.wav'])
-    subprocess.call(['ffmpeg', '-i', '../audio/test.wav', '-b:a', '16k', './audio/test2.wav'])
